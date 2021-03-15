4dfec4da376deccb4a11093acba0ceee88515983
Haltarys <gregoire.haardt@gmail.com>
2021-03-15 17:44:17 +0100

    feat: connect to MongoDB atlas database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfilesModule } from './profiles/profiles.module';
+import {
+  DB_URL,
+  DB_NAME,
+  DB_ADMIN_USER,
+  DB_ADMIN_PASSWORD,
+} from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(DB_URL, {
+      dbName: DB_NAME,
+      auth: {
+        user: DB_ADMIN_USER,
+        password: DB_ADMIN_PASSWORD,
+      },
+      retryWrites: true,
+      writeConcern: {
+        w: 'majority',
+      },
+    }),
+    ProfilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/config.ts
@@ -0,0 +1,7 @@
+const PORT = 3000;
+const DB_URL = process.env.DB_URL;
+const DB_NAME = process.env.DB_NAME;
+const DB_ADMIN_USER = process.env.DB_ADMIN_USER;
+const DB_ADMIN_PASSWORD = process.env.DB_ADMIN_PASSWORD;
+
+export { PORT, DB_URL, DB_NAME, DB_ADMIN_USER, DB_ADMIN_PASSWORD };
